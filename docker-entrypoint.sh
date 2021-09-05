46c01c11834734511bba6d4c2a3d5da74f1fc979
Bazmahou <bouissihamza6@gmail.com>
2021-09-05 16:06:15 +0100

    docker integration :rocket:

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 pipenv install
-pipenv shell
-python3 manage.py makemigrations --no-input
-python3 manage.py migrate --no-input
+pipenv run python manage.py makemigrations
+pipenv run python manage.py migrate
 chmod 777 media
 # Dev Or Product
 
 if $DOCKER_DEV
 then
-    python manage.py runserver 0.0.0.0:8000
+    pipenv run python manage.py runserver 0.0.0.0:8000
 else
-    python3 manage.py collectstatic --noinput
+    pipenv run python manage.py collectstatic --noinput
     # uwsgi --ini uwsgi.ini
     NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program uwsgi --ini uwsgi.ini
 fi
